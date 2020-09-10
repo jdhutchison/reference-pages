923957c266144a72269c25d47411faeb9908a714
jdhutchison <hutch@hutch.id.au>
2020-09-11 09:11:35 +1000

    Adding extract script.

---
+++ processing/extract_and_upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+###############################################################################
+###############################################################################
+###                                                                         ###
+###                 MARKDOWN EXTRACT AND UPLOAD SCRIPT                      ###
+###                                                                         ###
+### Extracts markdown from Joplin, calls the processing script and then     ###
+### uploads to Git.                                                         ###
+##  This script has no command line params.                                 ###
+###############################################################################
+###############################################################################
+
+REF_PAGES_HOME='/data/code/reference-pages'
+DATE=$(date +%F)
+
+rm -rf /tmp/General
+joplin export /tmp --notebook=General --format=md
+python3 $REF_PAGES_HOME/processing/markdown_processor.py
+cd $REF_PAGES_HOME/pages/content
+git add *
+git commit -m "Automatic upload on $DATE"
+git push
+
