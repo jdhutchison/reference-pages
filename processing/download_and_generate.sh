1a14cf48cba347531513d6c1854dc3e22884e9b8
jdhutchison <hutch@hutch.id.au>
2020-09-11 09:38:39 +1000

    Adding server-side processing script

---
+++ processing/download_and_generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###############################################################################
+###############################################################################
+###                                                                         ###
+###               MARKDOWN DOWNLOAD AND GENERATE SCRIPT                     ###
+###                                                                         ###
+### Pulls repository from Git and generates static content. To be run on    ###
+### the server. Hugp and Git must be installed for this to run.             ###
+### This script has no command line params.                                 ###
+###############################################################################
+###############################################################################
+
+
+WWW_FOLDER='/var/www/hutch.id.au/ref'
+HUGO_ROOT='/tmp'
+
+# Tidyup - remove old copies
+rm -r $WWW_FOLDER/*
+cd $HUGO_ROOT
+git clone https://github.com/jdhutchison/reference-pages.git
+cd reference-pages
+hugo -D --theme solar-theme-hugo -d $WWW_FOLDER
+
+# Can remove repo
+rm -r $HUGO_ROOT/reference-pages
+
